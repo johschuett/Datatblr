439cac5f6faba76026090c685b94204c5931b81d
Johannes Schütt <johschuett@uni-potsdam.de>
2020-07-07 15:16:45 +0200

    Update

--- main.R
+++ main.R
@@ -12,18 +12,16 @@
 # Packages
 library(dplyr)
 library(pkgcond)
-library(rapport)
-library(rapportools)
 library(rio)
 
 # Scripts
-if (file.exists("import.R")) { source("import.R") } else { stop("Could not call import.R!") }
+source("import.R")
 
 ## @knitr main
 
-if (file.exists("functions.R")) { source("functions.R") } else { stop("Could not call functions.R!") }
-if (file.exists("prep_output.R")) { source("prep_output.R") } else { stop("Could not call prep_output.R!") }
-if (file.exists("gen_f_m.R")) { source("gen_f_m.R") } else { stop("Could not call gen_f_m.R!") }
-if (file.exists("gen_l.R")) { source("gen_l.R") } else { stop("Could not call gen_l.R!") }
-if (file.exists("gen_n.R")) { source("gen_n.R") } else { stop("Could not call gen_n.R!") }
-if (file.exists("print_output.R")) { source("print_output.R") } else { stop("Could not call print_output.R!") }
+source("functions.R")
+source("prep_output.R")
+source("gen_f_m.R")
+source("gen_l.R")
+source("gen_n.R")
+source("print_output.R")
