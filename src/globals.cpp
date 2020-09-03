94efdec84546af54d278d210375fa7488158f66c
Johannes Schütt <johschuett@uni-potsdam.de>
2020-09-03 22:05:28 +0200

    Edit version

--- src/globals.cpp
+++ src/globals.cpp
@@ -4,4 +4,4 @@
 bool quit_flag = false;
 string current_dir;
 const string sep = "#! ---------------------------------------------------";
-const string version = "stable-1.0";
+const string version = "dev-1.1";
