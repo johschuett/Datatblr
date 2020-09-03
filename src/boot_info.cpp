db070c87b443a1adbdb27e66afba7e79dd8e6ee5
Johannes Schütt <johschuett@uni-potsdam.de>
2020-09-03 22:05:06 +0200

    Remove text from arg mode boot

--- src/boot_info.cpp
+++ src/boot_info.cpp
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include "utils.h"
 
-void boot_info()
+void boot_info(bool& args_given)
 {
   // Get working directory and write it into .current_dir file
   current_dir = "NULL";
@@ -38,18 +38,27 @@ void boot_info()
   // Print infotext
   cout << "#!" << bold_on << "         Hej, this is Datatblr " << version << "!" << bold_off << endl
     << "#!" << endl
-    << "#! License: GNU General Public License v2.0 only" << endl
-    << "#! Issue :a to see the authors of this program" << endl
-    << "#!" << endl
+    << "#! License: GNU General Public License v2.0 only" << endl;
+
+  if (!args_given)
+  {
+    cout << "#! Issue :a to see the authors of this program" << endl;
+  }
+
+  cout << "#!" << endl
     << "#!" << " Your R working directory is currently set to" << endl
     << "#!" << endl
     << "#! " << bold_on << current_dir << bold_off << endl
-    << "#!" << endl
-    << "#! Issue " << bold_on << ":w" << bold_off << " to learn how to change it." << endl
-    << "#!" << endl
-    << "#! Issue " << bold_on << ":e" << bold_off << " for examples on how to use the program." << endl
-    << "#! Issue " << bold_on << ":q" << bold_off << " to quit the program." << endl
-    << "#!" << endl
-    << "#! Please put in the path of your " << bold_on << "data" << bold_off << " file" << endl
-    << "#!" << bold_on << " or a command" << bold_off << " (starting with a colon):" << endl;
+    << "#!" << endl;
+
+  if (!args_given)
+  {
+    cout << "#! Issue " << bold_on << ":w" << bold_off << " to learn how to change it." << endl
+      << "#!" << endl
+      << "#! Issue " << bold_on << ":e" << bold_off << " for examples on how to use the program." << endl
+      << "#! Issue " << bold_on << ":q" << bold_off << " to quit the program." << endl
+      << "#!" << endl
+      << "#! Please put in the path of your " << bold_on << "data" << bold_off << " file" << endl
+      << "#!" << bold_on << " or a command" << bold_off << " (starting with a colon):" << endl;
+  }
 }
