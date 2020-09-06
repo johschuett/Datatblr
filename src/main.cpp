2866f22474000a44a0e664a8076e8c70b3fc75b0
Johannes Schütt <johschuett@uni-potsdam.de>
2020-09-06 14:59:14 +0200

    Edit ping command

--- src/main.cpp
+++ src/main.cpp
@@ -84,7 +84,7 @@ int main(int argc, char* argv[])
   // Checking R packages (dplyr, rio) and install the missing ones
 
   // Check internet connection
-  int connection = system("ping -c 1 cran.us.r-project.org &>/dev/null");
+  int connection = system("ping -c 1 cran.us.r-project.org > /dev/null");
   if (connection != 0)
   {
     // No internet connection, just check for the packages
